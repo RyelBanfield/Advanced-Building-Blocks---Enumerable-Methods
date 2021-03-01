26a647c0b952270067e8c3e3fba89f2f7ecefd9e
Arinpe <dehtolah@gmail.com>
2021-03-01 14:40:42 +0100

    Completed the other testing

--- spec/testing.rb
+++ spec/testing.rb
@@ -41,5 +41,33 @@ describe Enumerable do
     end
   end
 
+  describe 'my_none?' do
+    it 'returns false if none of the items are true' do
+      expect(num_arr.my_none? { |item| item.is_a? Integer }).to be false
+    end
+    it 'returns true if none of the items are true' do
+      expect(num_arr.my_none? { |item| item.is_a? String }).to be true
+    end
+  end
+  describe 'my_count' do
+    it 'returns the number of items' do
+      expect(num_arr.my_count).to eql(5)
+    end
+  end
+  describe 'my_map' do
+    it 'returns the modified array' do
+      expect(num_arr.my_map { 1 }).to eql([1, 1, 1, 1, 1])
+    end
+  end
+  describe 'my_inject' do
+    it 'returns the total sum of each array item' do
+      expect(num_arr.my_inject { |sum, item| sum + item }).to eql(15)
+    end
+  end
+  describe '#multiply_els' do
+    it 'returns the mulitple of each item in array' do
+      expect(multiply_els([1, 2, 3, 4, 5])).to eql(120)
+    end
+  end
   
 end
